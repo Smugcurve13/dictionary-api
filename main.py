22e26162db35df7488365a7b0fb4a104642ff4a9
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-10 16:02:12 +0530

    defination now is shown. dictionary api is done

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
-
+df = pd.read_csv('dictionary.csv')
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/api/v1/<word>')
 def dictionary(word):
+    defination = df.loc[df['word'] == word]['definition'].squeeze()
+
+    # defination = defination.to_json()
     return {
-        'defination': word.upper(),
+        'defination': defination,
         'word' : word
     }
 
